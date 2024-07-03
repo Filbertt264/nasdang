d6b8817a02bf2654697a7f5c0e57de0b37337ac5
Filbertt264 <filbertbaqir50@gmail.com>
2024-07-03 04:43:39 -0700

    Add files via upload

---
+++ website jual beli/include/footer.php
@@ -0,0 +1,42 @@
+      <footer class="footer">
+          <div class="container">
+
+
+              <div class="bottom-footer">
+                  <div class="row">
+                      <div class="col-xs-12 col-sm-3 payment-options color-black">
+                          <h5>Payment Options</h5>
+                          <ul>
+                              <li>
+                                  <a href="#"> <img src="images/gopay.png" alt="Gopay" width="35px"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/ovo.png" alt="Ovo" width="35px"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/Dana.png" alt="Dana" width="35px"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/spay.png" alt="Spay" width="35px"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/qris.png" alt="Qris" width="70px"> </a>
+                              </li>
+                          </ul>
+                      </div>
+                      <div class="col-xs-12 col-sm-4 address color-gray">
+                          <h5>Address</h5>
+                          <p>Purwokerto, Indonesia</p>
+                          <h5>Phone: +6201608445456</a></h5>
+                      </div>
+                      <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                          <h5>Addition informations</h5>
+                          <p>Join thousands of other restaurants who benefit from having partnered with us.</p>
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+      </footer>
+
+
